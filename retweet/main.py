e17daffd9de39b31899cc9d3333e073aa0e7a25e
Carl Chenet <chaica@ohmytux.com>
2015-09-08 16:17:52 +0200

    split configuration parsing to a dedicated module

--- retweet/main.py
+++ retweet/main.py
@@ -23,51 +23,36 @@ import tweepy
 
 from retweet.waitamoment import WaitAMoment
 from retweet.cliparse import CliParse
+from retweet.confparse import ConfParse
 
 
 class Main(object):
     '''Main class'''
     def __init__(self):
         '''Constructor of the Main class'''
-        self.user_to_retweet = 'journaldupirate'
-        consumer_key = ''
-        consumer_secret = ''
-        access_token = ''
-        access_token_secret = ''
-        self.lasttweetidfile = 'lastsenttweetid'
         rtargs = CliParse()
         pathtoconf = rtargs.configfile
         # read the configuration file
-        config = configparser.ConfigParser()
-        try:
-            with open(pathtoconf) as conffile:
-                config.readfp(conffile)
-                if config.has_section('main'):
-                    self.user_to_retweet = config.get('main', 'screen_name_of_the_user_to_retweet')
-                    consumer_key = config.get('main', 'consumer_key')
-                    consumer_secret = config.get('main', 'consumer_secret')
-                    access_token = config.get('main', 'access_token')
-                    access_token_secret = config.get('main', 'access_token_secret')
-                    self.lasttweetidfile = config.get('main', 'last_sent_tweet_id_file')
-        except (configparser.Error, IOError, OSError) as err:
-            print(err)
-            sys.exit(1)
+        cfgparse = ConfParse(pathtoconf)
+        self.cfgvalues = cfgparse.confvalues
 
         # activate the twitter api
-        self.auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+        self.auth = tweepy.OAuthHandler(self.cfgvalues['consumer_key'],
+                                        self.cfgvalues['consumer_secret'])
         self.auth.secure = True
-        self.auth.set_access_token(access_token, access_token_secret)
+        self.auth.set_access_token(self.cfgvalues['access_token'],
+                                    self.cfgvalues['access_token_secret'])
         self.api = tweepy.API(self.auth, wait_on_rate_limit=True, wait_on_rate_limit_notify=True)
         self.main()
 
     def main(self):
         '''Main of the Main class'''
         # get the 20 last tweets
-        lasttweets = self.api.user_timeline(self.user_to_retweet)
+        lasttweets = self.api.user_timeline(self.cfgvalues['user_to_retweet'])
 
-        if os.path.exists(self.lasttweetidfile) and os.path.isfile(self.lasttweetidfile):
+        if os.path.exists(self.cfgvalues['lasttweetidfile']) and os.path.isfile(self.cfgvalues['lasttweetidfile']):
             # a file with the last sent tweet id exists, using it
-            with open(self.lasttweetidfile) as desc:
+            with open(self.cfgvalues['lasttweetidfile']) as desc:
                 lasttweetid = int(desc.read())
             print("last sent tweet:{}".format(lasttweetid))
         else:
@@ -90,5 +75,5 @@ class Main(object):
                 WaitAMoment()
             # if we really sent tweets, store the last one
             if len(tweetstosend) != 0:
-                with open(self.lasttweetidfile, 'w') as desc:
+                with open(self.cfgvalues['lasttweetidfile'], 'w') as desc:
                     desc.write(str(tweetstosend[-1]))
