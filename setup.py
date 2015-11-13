10fea57b6e654866873b8e9f163d1a3f6cdeefa8
Carl Chenet <chaica@ohmytux.com>
2015-11-13 15:31:04 +0100

    bump version number

--- setup.py
+++ setup.py
@@ -28,7 +28,7 @@ CLASSIFIERS = [
 
 setup(
     name='retweet',
-    version='0.4',
+    version='0.5',
     license='GNU GPL v3',
     description='twitter bot to retweet all tweets from a user',
     long_description='twitter bot to retweet all tweets from a user',
