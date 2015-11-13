c401d691af1609119d1d227e62926fb39e9bffe8
Carl Chenet <chaica@ohmytux.com>
2015-11-13 16:34:35 +0100

    remove manual

--- setup.py
+++ setup.py
@@ -40,7 +40,6 @@ setup(
     url='https://github.com/chaica/retweet',
     download_url='https://github.com/chaica/retweet',
     packages=['retweet'],
-    data_files=[(os.path.join('share', 'man', 'man1'), ['man/retweet.1'])],
     scripts=['scripts/retweet'],
     install_requires=['tweepy>=3.3.0', 'sqlalchemy'],
 )
